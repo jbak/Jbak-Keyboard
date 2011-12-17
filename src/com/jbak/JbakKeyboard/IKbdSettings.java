8426be03f373c8cd039afd6f64ee0b226ee24c05
Bakunin <bakunin@corp.mail.ru>
2011-12-17 10:05:57 +0200

    Забыл залить новые файлы

---
+++ src/com/jbak/JbakKeyboard/IKbdSettings.java
@@ -0,0 +1,81 @@
+package com.jbak.JbakKeyboard;
+/** Константы для настроек клавиатуры */
+public interface IKbdSettings
+{
+	//-------------------------------------------------------------------    
+	/// Ключи для SharedPreferences
+	//-------------------------------------------------------------------    
+
+/** Ключ, boolean, хранящий значение "включить/отключить просмотр клавиш" */    
+    public static final String PREF_KEY_PREVIEW = "ch_preview";
+/** Ключ, int ,хранящий код последней используемой клавиатуры */    
+    public static final String PREF_KEY_LAST_LANG = "lastLng";
+/** Ключ, int, хранящий высоту клавиш в портретном режиме */    
+    public static final String PREF_KEY_HEIGHT_PORTRAIT = "kh";
+/** Ключ, int, хранящий высоту клавиш в ландшафтном режиме */    
+    public static final String PREF_KEY_HEIGHT_LANDSCAPE = "khl";
+/** Ключ, int, хранящий высоту клавиш по умолчанию, на всякий случай */    
+    public static final String PREF_KEY_DEF_HEIGHT = "dh";
+/** Ключ, String, хранящий порядок переключения языков */    
+    public static final String PREF_KEY_LANGS = "langs";
+/** Ключ, boolean, хранящий настройку вибро при коротком нажатии */    
+    public static final String PREF_KEY_VIBRO_SHORT_KEY = "vs";
+/** Ключ, boolean, хранящий настройку вибро при коротком нажатии */    
+    public static final String PREF_KEY_VIBRO_LONG_KEY = "vl";
+/** Ключ, boolean, хранящий настройку проигрывания звуков */    
+    public static final String PREF_KEY_SOUND = "sound";
+/** Ключ, int, хранящий ресурс клавиатуры для выбраного языка. Полный ключ выглядит как PREF_KEY_LANG_KBD+"en"*/    
+    public static final String PREF_KEY_LANG_KBD = "lkbd_";
+/** Ключ, int, хранящий индекс текущего скина*/    
+    public static final String PREF_KEY_KBD_SKIN = "kbd_skin";
+
+	//-------------------------------------------------------------------    
+	/// Константы для запуска настроечных активностей
+	//-------------------------------------------------------------------    
+    
+/** Значение для запуска {@link SetKbdActivity} - настройка высоты клавиш в портретном режиме */    
+    public static final int SET_KEY_HEIGHT_PORTRAIT = 1;
+/** Значение для запуска {@link SetKbdActivity} - настройка высоты клавиш в ландшафтном режиме */    
+    public static final int SET_KEY_HEIGHT_LANDSCAPE =2;
+/** Вызывает настройку переключения языков */    
+    public static final int SET_LANGUAGES_SELECTION =3;
+/** Вызывает настройку клавиш*/    
+    public static final int SET_KEYS =4;
+/** Вызывает настройку вида клавиатуры (обычный, для планешета...)*/    
+    public static final int SET_SELECT_KEYBOARD = 5;
+    /** Вызывает настройку внешнего вида клавиатуры (стандартный, айфон..)*/    
+    public static final int SET_SELECT_SKIN= 6;
+    
+	//-------------------------------------------------------------------    
+	/// Список команд 
+	//-------------------------------------------------------------------    
+
+ /** Внутреняя команда - голосовой ввод */   
+    public static final int CMD_VOICE_RECOGNIZER = -1;
+/** Внутреняя команда - запуск редактора шаблонов */    
+    public static final int CMD_TPL_EDITOR = -2;
+/** Внутреняя команда - показ шаблонов на клавиатуре */ 
+    public static final int CMD_TPL = -3;
+/** Внутреняя команда - запуск настроек */  
+    public static final int CMD_PREFERENCES = -4;
+/** Внутреняя команда - запуск мультибуфера обмена */   
+    public static final int CMD_CLIPBOARD = -5;
+/** Внутреняя команда - создание папки шаблонов */  
+    public static final int CMD_TPL_NEW_FOLDER = -6;
+
+  //-------------------------------------------------------------------    
+  /// Прочие строковые значения
+  //-------------------------------------------------------------------    
+    
+/** Значение для запуска {@link SetKbdActivity}. С этим ключом передаётся параметр типа int<br>
+ *  Параметр int - одно из значений SET_*/    
+    public static final String SET_INTENT_ACTION = "sa";
+ /**  Параметр String - название языка, для которого производится выбор клавиатуры */    
+    public static final String SET_INTENT_LANG_NAME = "sl";
+    
+/** Строковый префикс для кнопки, где метка для длинного нажатия является иконкой */
+    public static final String DRW_PREFIX = "d_"; 
+/** Тэг для записи в logcat*/
+    public static final String TAG = "JBK";
+
+}
